da2e8ca107ebea42e384bacff00d8f608e06951b
peterwainaina <petwah17@gmail.com>
2021-09-10 20:59:44 +0300

    Update :Apache Airflow Dag with python operator

--- Apache_Airflow/Getting started with Apache Airflow.md
+++ Apache_Airflow/Getting started with Apache Airflow.md
@@ -7,13 +7,27 @@ It will provide you an amazing user interface to monitor and fix any issues that
 ## Installation steps
 Let’s start with the installation of the Apache Airflow. Now, if already have pip installed in your system, you can skip the first command. To install pip run the following command in the terminal.
 ```bash
-sudo yum install python2-pip
+sudo yum install python3-pip
 ```
 Next airflow needs a home on your local system. By default ~/airflow is the default location but you can change it as per your requirement.
 ```bash
 export AIRFLOW_HOME=~/airflow
 ```
+Now, install the apache airflow using the pip with the following command.
+```bash
+pip3 install apache-airflow
+```
+
 Airflow requires a database backend to run your workflows and to maintain them. Now, to initialize the database run the following command.
 ```bash
 airflow db init
+```
+We have already discussed that airflow has an amazing user interface. To start the webserver run the following command in the terminal. The default port is 8080 and if you are using that port for something else then you can change it.
+
+```bash
+airflow webserver -p 8080
+```
+Now, start the airflow schedular using the following command in a different terminal. It will run all the time and monitor all your workflows and triggers them as you have assigned.
+```bash
+airflow scheduler
 ```
---
+++ Apache_Airflow/pythonoperator.py
@@ -0,0 +1,53 @@
+## importing aiflow libraries
+
+from datetime import timedelta
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.utils.dates import days_ago
+
+
+## Defining DAG Arguments
+default_args = {
+    'owner': 'lakshay',
+    'depends_on_past': False,
+    'start_date': days_ago(2),
+    'email': ['airflow@example.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+    # 'queue': 'bash_queue',
+    # 'pool': 'backfill',
+    # 'priority_weight': 10,
+    # 'end_date': datetime(2016, 1, 1),
+    # 'wait_for_downstream': False,
+    # 'dag': dag,
+    # 'sla': timedelta(hours=2),
+    # 'execution_timeout': timedelta(seconds=300),
+    # 'on_failure_callback': some_function,
+    # 'on_success_callback': some_other_function,
+    # 'on_retry_callback': another_function,
+    # 'sla_miss_callback': yet_another_function,
+    # 'trigger_rule': 'all_success'
+}
+
+# define the python function
+def my_function(x):
+    return x + " is a must have tool for Data Engineers."
+
+# define the DAG
+dag = DAG(
+    'python_operator_sample',
+    default_args=default_args,
+    description='How to use the Python Operator?',
+    schedule_interval=timedelta(days=1),
+)
+# define the first task
+t1 = PythonOperator(
+    task_id='print',
+    python_callable= my_function,
+    op_kwargs = {"x" : "Apache Airflow"},
+    dag=dag,
+)
+
+t1
