f0736897601d20dcd09fa51ce701d45542466de9
peterwainaina <petwah17@gmail.com>
2021-09-13 15:41:36 +0300

    ETL for get weather case study ,this has extraction bit,transformation bit and loading to database

--- Apache_Airflow/case_study/etl/extract_getWeather.py
+++ Apache_Airflow/case_study/etl/extract_getWeather.py
@@ -8,12 +8,16 @@ from datetime import datetime
 def fetch_weather_info():
     """This function will fetch openweathermap.com'S api AND  GET WEATHER FOR bROOKLYN,Ny AND then dump the json to the src/data directory
     with the file name "todays date".json" """
-
-    parameters={'q': 'Brooklyn, USA'}
-    result = requests.get("http://api.openweathermap.org/data/2.5/weather?,parameters")
+    API_KEY='e4e9cd51a9ecd44bb1929b8c4784df55'
+    print(API_KEY)
+    parameters={'q': 'Brooklyn, USA','appid':API_KEY}
+    print(parameters)
+    result = requests.get("http://api.openweathermap.org/data/2.5/weather?q=London&appid=e4e9cd51a9ecd44bb1929b8c4784df55",)
+    print("result is ",result)
 
     # if the API call was successful ,get the json and dump it to a file with todays date as title.
-    if result.status_code ==200:
+    if result.status_code == 200:
+        print("con")
         # Get json data
         json_data = result.json()
         file_name = str(datetime.now().date())+ '.json'
