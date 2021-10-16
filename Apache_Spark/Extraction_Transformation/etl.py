6e997b3a346a1f5b0a141365e94e9af716ac4f41
peterwainaina <petwah17@gmail.com>
2021-10-16 13:52:35 +0300

    Creating dictionary from data frame using collect function

--- Apache_Spark/Extraction_Transformation/etl.py
+++ Apache_Spark/Extraction_Transformation/etl.py
@@ -7,7 +7,8 @@ from Apache_Spark.setting.project_config import *
 
 ## spark session
 driver_path = "/home/peterwainaina/ETL_Project/resources/drivers/postgresql-42.2.23.jar"
-spark=SparkSession.builder.master(f"{os.environ.get('SPARK_MASTER')}")\
+# SparkSession.builder.master(f"{os.environ.get('SPARK_MASTER')}")
+spark=SparkSession.builder.master("local")\
     .appName("Geopro")\
     .config("spark.jars", driver_path)\
     .getOrCreate()
@@ -18,6 +19,18 @@ database_config=json.loads(os.environ.get('EDMS_V3'))
 """using subquery to read frrom postgres"""
 df = spark.read.jdbc(url = f"jdbc:postgresql://{database_config['host']}:{database_config['port']}/{database_config['database']}",
 
-                     table = "(SELECT document_type, file_number, folio_number, metadata, document FROM nairobi_central.registration_main_test) AS my_table",
-                     properties={"user": f"{database_config['user']}", "password": f"{database_config['password']}", "driver": 'org.postgresql.Driver'})
-print(df)
+                     table = "(SELECT document_type, file_number, folio_number, metadata, document FROM nairobi_central.registration_main_test where file_number='charge/200') AS my_table",
+                     properties={"user": f"{database_config['user']}", "password": f"{database_config['password']}", "driver": 'org.postgresql.Driver'})\
+    # .createTempView('tbl')
+
+
+"""using collect() function to loop through the dataframe"""
+
+##Storing in variable
+data_collect=df.collect()
+
+# looping through each of the dataframe
+for values in data_collect:
+    registration_dict={"document_type":values['document_type'],
+                       "metadata":values['metadata']}
+    print(registration_dict)
