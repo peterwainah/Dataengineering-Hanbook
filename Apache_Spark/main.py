e26e71efcd78fffcdf6f3f2b19d35b88322e75e5
peterwainaina <petwah17@gmail.com>
2021-10-18 09:47:59 +0300

    Bugfix for absolute path by importing base directory and appending to system

--- Apache_Spark/main.py
+++ Apache_Spark/main.py
@@ -1,6 +1,10 @@
 
 import sys
-sys.path.append('/home/peterwainaina/Dataengineering-Handbook/Apache_Spark/Extraction_Transformation')
-import etl
+import os
+base_dir=os.path.dirname(__file__) or '.'
+parent_dir_path=os.path.abspath(os.path.join(base_dir,os.pardir))
+sys.path.insert(0,parent_dir_path)
 
-etl.etl_spark()
+from Apache_Spark.Extraction_Transformation.etl import etl_spark
+
+etl_spark()
