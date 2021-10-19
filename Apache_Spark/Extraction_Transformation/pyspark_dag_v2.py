8cb583dd0e3affef9bec0846d31b28938702717e
peterwainaina <petwah17@gmail.com>
2021-10-19 07:54:24 +0300

    Creating pyspark dag using bash operator

---
+++ Apache_Spark/Extraction_Transformation/pyspark_dag_v2.py
@@ -0,0 +1,31 @@
+
+from datetime import timedelta
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.utils.dates import days_ago
+
+default_args={
+    'owner':'ADI',
+    'depends_on_post':False,
+    'email':['geoprodbteam@gmail.com'],
+    'email_on_failure':False,
+    'email_on_retry':False,
+    'retries':3,
+    'retry_delay':timedelta(minutes=5)
+
+}
+
+dag=DAG(
+    dag_id='transactable_delta',
+    default_args=default_args,
+    start_date=days_ago(0),
+    schedule_interval="2 * * * *",
+
+)
+pyspark_job_v2=BashOperator(
+    task_id='pyspark_job_v2',
+    bash_command="python3 /opt/Dataengineering-Handbook/Apache_Spark/main.py",
+    dag=dag
+)
+
+pyspark_job_v2
